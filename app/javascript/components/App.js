bc0b2cf451ee4498a56a5e68057b835055e41b40
mike <apolloproductionsdj@gmail.com>
2021-02-15 21:34:15 -0700

    set up components folder inside of javascript and created first <App /> to be rendered inside the index.jsx

---
+++ app/javascript/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+    return (
+        <div>
+            <h1>Hello React on Ruby on Rails project</h1>
+        </div>
+    )
+}
+
+export default App;
