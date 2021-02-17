daf9c13d60940cb0988ccf68415d42084935685f
mike <apolloproductionsdj@gmail.com>
2021-02-17 00:27:05 -0700

    added in useState and useEffect in order to get access to api and update state display dj companies in a <ul>

--- app/javascript/components/Djs/Djs.js
+++ app/javascript/components/Djs/Djs.js
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
+import axios from 'axios';
 
 function Djs() {
+
+    const [djs, setDjs] = useState([]);
+
+    useEffect(() => {
+        // Get all of our DJ's from our Api
+        axios.get('/api/v1/djs.json')
+
+            .then(resp => {
+                setDjs(resp.data.data)
+            })
+            .catch(resp => console.log(resp))
+        // Update all djs in our state
+    }, [djs.length])
+
+    const list = djs.map(item => {
+        return (<li key={item.id}>{item.attributes.name}</li>)
+    })
+
     return (
-        <div>
-            <h1>This is the Djs#index for our app</h1>
+        <Fragment>
+            <div>
+                This is the Airlines#index view for our app.
         </div>
+            <ul>{list}</ul>
+        </Fragment>
     )
 }
 
